4c95f63512c7b1ac7b3139fc9f2a6f08e8e43255
chenjie <chenjie@atominds.com>
2019-04-18 17:29:25 +0800

    首次提交

---
+++ src/main/java/com/dasorb/framework/base/DasorbFrameworkBaseApplication.java
@@ -0,0 +1,13 @@
+package com.dasorb.framework.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasorbFrameworkBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DasorbFrameworkBaseApplication.class, args);
+    }
+
+}
